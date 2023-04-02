408138352e7bd20fd77cc3f458721884cbc4db06
aliss <aliss@LAPTOP-5QN9CN36.local>
2023-04-02 19:06:44 -0300

    Repositorio usado para a matéria de TOO. Alisson...

---
+++ src/main/java/com/mycompany/java_maven_too_amr23/Java_maven_too_AMR23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_maven_too_amr23;
+
+/**
+ *
+ * @author aliss
+ */
+public class Java_maven_too_AMR23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
